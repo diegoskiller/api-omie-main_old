170206fed750f1d927638d28bcf7efdd45bba450
dyda91 <edjamasalmeida@outlook.com>
2024-01-09 22:59:02 -0300

    atualiza model

--- models/models.py
+++ models/models.py
@@ -122,7 +122,7 @@ class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=False)
 
